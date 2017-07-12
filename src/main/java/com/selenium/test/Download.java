956b71ea533ad128963212a4f87bc77f9750b48d
thinksysuser <thinksysuser@thinksysusers-MacBook-Pro.local>
2017-07-12 19:20:23 +0530

    comment added

--- src/main/java/com/selenium/test/Download.java
+++ src/main/java/com/selenium/test/Download.java
@@ -11,13 +11,14 @@ public class Download {
 	public static void main(String[] args) throws IOException, Exception {
 	System.setProperty("webdriver.chrome.driver", "chromedriver");
 	WebDriver driver = new ChromeDriver();
-	driver.get("http://www.smilebox.com/");
-	Thread.sleep(20000);
-	driver.findElement(By.xpath("//img[@src='/images/bt_downloadnow_normal.png']")).click();
-	Thread.sleep(20000);
-	String dip_url = driver.getCurrentUrl();
-	System.out.println("URL of the Download Instruction page is : ");
-	System.out.println(dip_url);
+	//driver.get("http://www.smilebox.com/");
+	//Thread.sleep(20000);
+	//driver.findElement(By.xpath("//img[@src='/images/bt_downloadnow_normal.png']")).click();
+	//Thread.sleep(20000);
+	//String dip_url = driver.getCurrentUrl();
+	//System.out.println("URL of the Download Instruction page is : ");
+	//System.out.println(dip_url);
+	
 	
 	}
 }
